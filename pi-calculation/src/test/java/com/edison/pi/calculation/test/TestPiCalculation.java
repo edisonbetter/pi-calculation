fefc67f8ad82501a285d51787b63aee580498c06
edisonyang <edisonyang@192.168.1.103>
2014-08-10 23:55:33 +0800

    update test cases

--- pi-calculation/src/test/java/com/edison/pi/calculation/test/TestPiCalculation.java
+++ pi-calculation/src/test/java/com/edison/pi/calculation/test/TestPiCalculation.java
@@ -2,6 +2,8 @@ package com.edison.pi.calculation.test;
 
 import java.math.BigDecimal;
 
+import junit.framework.Assert;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -78,11 +80,37 @@ public class TestPiCalculation {
 		outputResult(calculationService.getNumberOfTerms(), result);
 	}
 
-	//@Test
-	public void test_1_000_000_000_terms() {
-		calculationService.setNumberOfTerms(1000000000);
-		double result = calculationService.calculate();
-		outputResult(calculationService.getNumberOfTerms(), result);
+	@Test
+	public void test_10_to_100_terms() {
+		for (int i = 10; i < 100; i++) {
+			calculationService.setNumberOfTerms(i);
+			double result = calculationService.calculate();
+			BigDecimal difference = getDifference(result);
+			int accurateDecimalDigit = getAccurateDecimalDigits(difference);
+			Assert.assertTrue(accurateDecimalDigit >= 1);
+		}
+	}
+
+	@Test
+	public void test_100_to_1000_terms() {
+		for (int i = 100; i < 1000; i++) {
+			calculationService.setNumberOfTerms(i);
+			double result = calculationService.calculate();
+			BigDecimal difference = getDifference(result);
+			int accurateDecimalDigit = getAccurateDecimalDigits(difference);
+			Assert.assertTrue(accurateDecimalDigit >= 2);
+		}
+	}
+
+	@Test
+	public void test_1000_to_10000_terms() {
+		for (int i = 1000; i < 10000; i++) {
+			calculationService.setNumberOfTerms(i);
+			double result = calculationService.calculate();
+			BigDecimal difference = getDifference(result);
+			int accurateDecimalDigit = getAccurateDecimalDigits(difference);
+			Assert.assertTrue(accurateDecimalDigit >= 3);
+		}
 	}
 
 	private void outputResult(int terms, double result) {
@@ -97,6 +125,12 @@ public class TestPiCalculation {
 		System.out.println("Accurate Decimal Digits : "
 				+ getAccurateDecimalDigits(difference));
 		System.out.println();
+	}
+
+	private BigDecimal getDifference(double result) {
+		BigDecimal difference = BigDecimal.valueOf(result)
+				.subtract(BigDecimal.valueOf(PI)).abs();
+		return difference;
 	}
 
 	private int getAccurateDecimalDigits(BigDecimal value) {
