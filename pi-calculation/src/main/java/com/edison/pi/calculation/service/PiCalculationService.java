ca07bcf9bfd8e4e225b015d2f37908963737ef58
edisonyang <edisonyang@192.168.1.103>
2014-08-21 01:11:15 +0800

    add comments

--- pi-calculation/src/main/java/com/edison/pi/calculation/service/PiCalculationService.java
+++ pi-calculation/src/main/java/com/edison/pi/calculation/service/PiCalculationService.java
@@ -7,7 +7,21 @@ package com.edison.pi.calculation.service;
  * @date 2014-08-10 
  */
 public interface PiCalculationService {
+	/**
+	 * calculation logic should be put here
+	 * @return calculation result, approximation of Pi
+	 */
 	double calculate();
+	
+	/**
+	 * get number of terms in the calculation formula
+	 * 
+	 */
 	int getNumberOfTerms();
+	
+	/**
+	 * set number of terms in the calculaton formula
+	 * @param numberOfTerms
+	 */
 	void setNumberOfTerms(int numberOfTerms);
 }
